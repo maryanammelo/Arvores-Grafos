432e07e7895e3efb42cb67b863286d705451ea97
Maryana Machado Melo <160974841+maryanammelo@users.noreply.github.com>
2026-04-01 18:57:34 -0300

    Add comments to ex30_analiseCriticaAlgoritmos.c

---
+++ sem8/ex30_analiseCriticaAlgoritmos.c
@@ -0,0 +1,3 @@
+//I- Depende da quantidade de dados existentes na lista, caso a sequencial ja estiver ordenada e tiver ummenor numero de elementos, ha a possibilidade dela pode ser mais rapida.
+//II-É verdadeira, visto que lista pode estar desordenada, ou seja, nao estao em uma ordem especifica.
+//III- Sim, é verdadeira, ela requer que os dados estejam ordenados
