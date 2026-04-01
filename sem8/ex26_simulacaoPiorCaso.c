0f06aaba892c6bd3d72dd54866de89367918e8ba
Maryana Machado Melo <160974841+maryanammelo@users.noreply.github.com>
2026-04-01 18:20:12 -0300

    Document sequential search scenarios in simulation
    Add comments explaining worst and best case scenarios for sequential search.

---
+++ sem8/ex26_simulacaoPiorCaso.c
@@ -0,0 +1,3 @@
+//Na busca sequencial o vetor percorre posicao por posicao ate que o elemento seja encontrado. Logo no pior caso da busca sequencial
+// é quando o valor buscado esta na utima posicao, ou seja, ele ira percorrer todos ate chegar no que deseja, sendo nesse caso cerca de 500
+//comparacoes. Ja no melhor caso, é quando o elemento procurado esta na primeira posicao, ou seja, so é necessaria una comparacao.
