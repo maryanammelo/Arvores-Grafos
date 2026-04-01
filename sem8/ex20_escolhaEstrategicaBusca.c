588ad6c69a4f33b522126539691e2669efd2b5ba
Maryana Machado Melo <160974841+maryanammelo@users.noreply.github.com>
2026-04-01 17:22:58 -0300

    Add comments on binary search strategy
    Add comments explaining the use of binary search for large data volumes.

---
+++ sem8/ex20_escolhaEstrategicaBusca.c
@@ -0,0 +1,3 @@
+//Para esse cenario em que ha um grande volume de dados, e ele cresce constantemente. Convem, utilizar a busca binaria, de modo que antes que
+// ela seja executada,primeiramente os dados sejam ordenados e  em seguidda usa-se a busca binaria. Ja que ela é indicada para sistemas que
+//realizam muitas operacoes de busca, e tambem quando a velocidade de busca é critica.
